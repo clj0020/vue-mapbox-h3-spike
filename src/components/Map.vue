3778f815cd8bea17ce4a37d0d65e51e89c013c9c
Carson Jones <clj0020@auburn.edu>
2020-02-14 12:14:17 -0500

    Added resolution check.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,14 +1,39 @@
 <template>
-  <MglMap 
-    ref="map"
-    :accessToken="accessToken"
-    :mapStyle.sync="mapStyle"
-    @load="onMapLoaded">
-  </MglMap>
+<div>
+    <MglMap
+      id="map"
+      :accessToken="accessToken"
+      :mapStyle.sync="mapStyle"
+      @load="onMapLoaded">
+    </MglMap>
+    <div id="changeResolution">
+        <h3>Resolution: {{h3Resolution}}</h3>
+        <input        
+          type="range"
+          min="7"
+          v-bind:max="maxH3Resolution"
+          v-bind:value="h3Resolution"
+          v-on:input="onSliderChange($event)"/>
+    </div>
+</div>
 </template>
+
+<style>
+
+  #changeResolution {
+    position: relative; 
+    background: #ffffff; 
+    display: inline-block;  
+  }
+
+  #map {
+    position: fixed;
+  }
+</style>
 
 <script>
 import Mapbox from "mapbox-gl";
+import { json } from "d3-fetch";
 
 import { 
   MglMap
@@ -21,10 +46,19 @@ export default {
   data() {
     return {
       accessToken: 'pk.eyJ1IjoiY2xqMDAyMCIsImEiOiJjam80b2Y4cmEwMWFrM3ZwNW9wbzZvNjF0In0.tGdbiR2A0B9bbHXo_Hg93w', // your access token. Needed if you using Mapbox maps
-      mapStyle: 'mapbox://styles/mapbox/streets-v11' // your map style
+      mapStyle: 'mapbox://styles/mapbox/streets-v11', // your map style
+      config: {
+        lng: -122.2,
+        lat: 37.7923539,
+        zoom: 10.5,
+        fillOpacity: 0.75,
+        colorScale: ['#ffffD9', '#50BAC3', '#1A468A']
+      },
+      maxH3Resolution: 10,
+      h3Resolution: 7,
+      travelTimes: null
     };
   },
-
   created() {
     // We need to set mapbox-gl library here in order to use it in template
     this.mapbox = Mapbox;
@@ -34,27 +68,30 @@ export default {
     async onMapLoaded(event) {
       this.map = event.map;
 
-      const config = ({
-        lng: -84.373684,
-        lat: 33.820379,
-        zoom: 11.5,
-        fillOpacity: 0.6,
-        colorScale: ['#ffffcc', '#78c679', '#006837']
-      })
+      console.log(this.map);
 
       const asyncActions = event.component.actions;
       
       const newParams = await asyncActions.flyTo({
-        center: [-84.373684, 33.820379],
-        zoom: 11.5,
+        center: [this.config.lng, this.config.lat],
+        zoom: this.config.zoom,
         speed: 1
       });
       console.log(newParams);
 
-      const centerHex = this.$geoToH3(33.819310, -84.381538, 8);
-      const kRing = this.$kRing(centerHex, 3);
-
-      const hexagons = kRing.reduce((res, hexagon) => ({...res, [hexagon]: Math.random()}), {});
+      this.travelTimes = await this.seedTravelTimes();
+      console.log(this.travelTimes);
+      const hexagons = this.transformPolygonsToHexagons(this.travelTimes);
+      this.renderHexes(this.map, hexagons);
+    },
+    onSliderChange(event) {
+      console.log(typeof event.target.value);
+      console.log(event.target.value);
+      this.h3Resolution = Number(event.target.value);
+      const hexagons = this.transformPolygonsToHexagons(this.travelTimes);
+      this.renderHexes(this.map, hexagons);
+    },
+    renderHexes(map, hexagons) {
       // Transform the current hexagon map into a GeoJSON object
       const geojson = this.$geojson2h3.h3SetToFeatureCollection(
         Object.keys(hexagons),
@@ -64,7 +101,7 @@ export default {
       const sourceId = 'h3-hexes';
       const layerId = `${sourceId}-layer`;
 
-      let source = this.map.getSource(sourceId);
+      let source = map.getSource(sourceId);
 
       if(!source) {
         const geoJsonLayer = {
@@ -77,29 +114,59 @@ export default {
             }
         };
 
-      
-        this.map.addSource(sourceId, {
+        map.addSource(sourceId, {
           type: 'geojson',
           data: geojson
         });
-        this.map.addLayer(geoJsonLayer);
+        map.addLayer(geoJsonLayer);
 
-        source = this.map.getSource(sourceId);
+        source = map.getSource(sourceId);
       }
     
       source.setData(geojson);
 
       // Update the layer paint properties, using the current config values
-      this.map.setPaintProperty(layerId, 'fill-color', {
+      map.setPaintProperty(layerId, 'fill-color', {
         property: 'value',
         stops: [
-          [0, config.colorScale[0]],
-          [0.5, config.colorScale[1]],
-          [1, config.colorScale[2]]
+          [0, this.config.colorScale[0]],
+          [0.5, this.config.colorScale[1]],
+          [1, this.config.colorScale[2]]
         ]
       });
   
-      this.map.setPaintProperty(layerId, 'fill-opacity', config.fillOpacity);
+      map.setPaintProperty(layerId, 'fill-opacity', this.config.fillOpacity);
+    },
+    generateBasicHexagons() {
+      const centerHex = this.$geoToH3(this.config.lat, this.config.lng, 8);
+      const kRing = this.$kRing(centerHex, 3);
+      return kRing.reduce((res, hexagon) => ({...res, [hexagon]: Math.random()}), {});
+    },
+    seedTravelTimes() {
+      return json('https://gist.githubusercontent.com/nrabinowitz/d3a5ca3e3e40727595dd137b65058c76/raw/657a9f3b64fedc718c3882cd4adc645ac0b4cfc5/oakland_travel_times.json');
+    },
+    transformPolygonsToHexagons(travelTimes) {
+        const layer = {};
+        travelTimes.features.forEach(feature => {
+          const hexagons = this.$geojson2h3.featureToH3Set(feature, this.h3Resolution);
+            hexagons.forEach(h3Index => {
+            layer[h3Index] = feature.properties.travelTime;
+          })
+        });
+        return this.normalizeLayer(layer);
+    },
+    normalizeLayer(layer, zeroBaseline = false) {
+      const hexagons = Object.keys(layer);
+      // Pass one, get max (and min if needed)
+      const max = hexagons.reduce((max, hex) => Math.max(max, layer[hex]), -Infinity);
+      const min = zeroBaseline ? 0 : hexagons.reduce((min, hex) => Math.min(min, layer[hex]), Infinity);
+
+      // Pass two, normalize.
+      hexagons.forEach(hex => {
+        layer[hex] = (layer[hex] - min) / (max - min);
+      });
+
+      return layer;
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import geojson2h3 from 'geojson2h3';
 import { geoToH3, kRing } from 'h3-js';
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Object.defineProperty(Vue.prototype, '$geojson2h3', { value: geojson2h3 });
 Object.defineProperty(Vue.prototype, '$geoToH3', { value: geoToH3 });
 Object.defineProperty(Vue.prototype, '$kRing', { value: kRing });
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
